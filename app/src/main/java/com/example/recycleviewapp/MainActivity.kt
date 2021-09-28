f3561ce555d135fe4c6451899ce488059ad502e7
iam-that-one <iam-that-one@users.noreply.github.com>
2021-09-28 19:22:01 +0300

    some-fixed

--- app/src/main/java/com/example/recycleviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewapp/MainActivity.kt
@@ -13,70 +13,75 @@ import kotlin.random.Random
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-    lateinit var add : Button
-    lateinit var txt : EditText
-    lateinit var lbl : TextView
-    lateinit var  lbl1 : TextView
-    lateinit var clear : Button
+    lateinit var guess: Button
+    lateinit var txt: EditText
+    lateinit var lbl: TextView
+    lateinit var lbl1: TextView
+    lateinit var clear: Button
+
+
     var message = ""
-     var tarace = listOf("HI THERE", "YOU ARE WELCOME","KOTLIN APPLICATION", "HI LOVLY")
+    val phrases = listOf("HI THERE", "YOU ARE WELCOME", "KOTLIN APPLICATION", "HI LOVLY ONE")
+    var rand: Int = 0
+    var randPhrase: String = ""
     var messages = mutableListOf<String>()
+
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txt = findViewById(R.id.editTextTextPersonName3)
-
-        add = findViewById(R.id.button2)
+        guess = findViewById(R.id.button2)
         lbl1 = findViewById(R.id.textView)
         clear = findViewById(R.id.button1)
         clear.alpha = 1F
+
         val myRV = findViewById<RecyclerView>(R.id.rvMain)
         myRV.adapter = RecycleViewAddapter(messages)
         myRV.layoutManager = LinearLayoutManager(this)
         var ans = ""
-        var rand = Random.nextInt(tarace.size-1)
-        var randPhrase = tarace[rand]
         var hint = ""
-        for (i in 0..randPhrase.length-1){
-            if(randPhrase[i] != ' '){
-                hint = hint + "*"
-            }else{
-                hint = hint + randPhrase[i]
-            }
-        }
-        lbl1.text = "Guess the correct phrase [${hint}]"
-        clear.setOnClickListener{
-            lbl1.text = "Guess the correct phrase [${hint}]"
-            clear.alpha = 1F
-            messages.clear()
-            myRV.adapter?.notifyDataSetChanged()
-        }
-        add.setOnClickListener {
-                ans = txt.text.toString()
-                rand = Random.nextInt(tarace.size - 1)
-                // var randomPhrase = tarace[rand]
-                if (ans == randPhrase) {
-                    lbl1.text = "Guess the correct phrase [Unlocked] [${ans}]"
-                    clear.alpha = 1F
-                    messages.add("YOU GOT THE PHRASE")
+        var str = ""
+        clear.setOnClickListener {
+            str = ""
+            lbl1.text = ""
+            rand = Random.nextInt(phrases.size)
+            randPhrase = phrases[rand]
+            for (i in 0..randPhrase.length - 1) {
+                if (randPhrase[i] != ' ') {
+                    hint = hint + "*"
                 } else {
-                    var str = ""
-                    for (i in 0..randPhrase.length - 1) {
-                        if (randPhrase[i] == ans[i]) {
-                            str = str + randPhrase[i]
-                        } else {
-                            str = str + "*"
-                        }
-                        if (ans.length > randPhrase.length || ans.length < randPhrase.length) {
-                            str = "Miss match length, try again ${hint}"
-                            break
-                        }
-                    }
-                    lbl1.text = str
+                    hint = hint + " "
                 }
-                myRV.adapter?.notifyDataSetChanged()
-                txt.text.clear()
+            }
+
+            lbl1.text = hint
+        }
+        guess.setOnClickListener {
+            ans = txt.text.toString()
+
+            if (ans == randPhrase) {
+                lbl1.text = "Guess the correct phrase [Unlocked] [${ans}]"
+                messages.add("YOU GOT THE PHRASE")
+                hint = ""
+            }
+            else{
+                for (i in 0..randPhrase.length - 1) {
+                    if (randPhrase[i] == ans[i]) {
+                        str = str + randPhrase[i]
+                    } else {
+                        str = str + "*"
+                    }
+                    if (ans.length > randPhrase.length || ans.length < randPhrase.length) {
+                        hint = ""
+                        str = "Miss match length, try again ${hint}"
+                        break
+                    }
+                }
+                lbl1.text = str
+            }
+            myRV.adapter?.notifyDataSetChanged()
+            txt.text.clear()
 
         }
     }
